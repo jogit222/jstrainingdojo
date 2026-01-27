be0406cecb85ca016e90da98f705b98b8eb3534a
jogit222 <joris.bastiaens@leerling.busokristuskoning.be>
2026-01-27 13:43:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
  
 
 function changeText()    {
-  greeting-text.innerText = 'Hello, welcome to Javascript Dojo'
+  greetingText.innerText = 'Hello, welcome to Javascript Dojo'
 }
 
 if (greeting-text)   {
-  const greeting-text = document.getElementById('greeting-text')
-  const greeting-text-button = document.getElementById('btn-change-text')
-  greeting-text-button.addEventListener('click', changeText)
+  const greetingText = document.getElementById('greeting-text')
+  const greetingText-button = document.getElementById('btn-change-text')
+  greetingTextButton.addEventListener('click', changeText)
