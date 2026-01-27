f385a1d18b0ca5e6130c46c688637a1df4d55a26
jogit222 <joris.bastiaens@leerling.busokristuskoning.be>
2026-01-27 13:36:21 +0100

    Update app.js
    added the greeting-text functionality

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const greeting-text = document.getElementById('greeting-text')
 
+
+
+function changeText()    {
+  greeting-text.innerText = 'Hello, welcome to Javascript Dojo'
+}
+
+if (greeting-text)   {
+  const greeting-text-button = document.getElementById('btn-change-text')
+  greeting-text-button.addEventListener('click', changeText)
